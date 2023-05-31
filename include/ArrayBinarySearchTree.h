0f857aadddfd6cc68728d96b0498b3e46e7e96f0
RohitMandal <mandal123rohit@gmail.com>
2023-05-31 23:19:12 +0545

    completed

---
+++ include/ArrayBinarySearchTree.h
@@ -0,0 +1,27 @@
+#ifndef ArrayBinarySearchTree_H
+#define ArrayBinarySearchTree_H
+#include "./AbstractBST.h"
+#define MAX_NUM_NODES 128
+
+class ArrayNode
+{
+public:
+    int key;
+    int value;
+    ArrayNode(int key, int value) : key(key), value(value) {}
+};
+
+class ArrayBST : public AbstractBST
+{
+public:
+    int size;
+    ArrayNode *node[MAX_NUM_NODES];
+    ArrayBST();
+    ~ArrayBST();
+    bool isEmpty();
+    void addBST(int key, int value);
+    void removeBST(int targetkey);
+    int searchBST(int targetKey);
+};
+
+#endif
